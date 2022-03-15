19c2e8b5165afa8c8b2bdd09b887ab4ca894c248
Roadman-Lee <rdmshine@gmail.com>
2022-03-15 09:39:49 +0900

    modify : router, detail response file name

--- content_post/apis/v1/detail_router.py
+++ content_post/apis/v1/detail_router.py
@@ -2,22 +2,24 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from ninja import Router
 
-from content_post.apis.v1.schemas.DetailResponse import DetailResponse
+from content_post.apis.v1.schemas.detail_response import DetailResponse
 
-detail = Router(tags=["Content_CRUD"])
+content = Router(tags=["Content_CRUD"])
 
 
-@detail.get("", response=DetailResponse)
+# detail page render router
+@content.get("", response=DetailResponse)
 def get_detail_page(request: HttpRequest) -> HttpResponse:
     return render(request, "detail.html")
 
 
-@detail.get("/feeds/", response=DetailResponse)
+# detail/feeds page render router
+@content.get("/feeds/", response=DetailResponse)
 def get_feeds_page(request: HttpRequest) -> HttpResponse:
     return render(request, "add.html")
 
 
-# 수정페이지 이동 변경예정
+# # 수정페이지 이동 변경예정
 # @detail.get('/feeds/', response=DetailResponse)
 # def get_update_page(request: HttpRequest):
 #     return render(request, 'add.html')
--- user_admission/apis/v1/register_router.py
+++ user_admission/apis/v1/register_router.py
@@ -4,10 +4,10 @@ from ninja import Router
 
 from user_admission.apis.v1.schemas.register_response import RegisterResponse
 
-register = Router(tags=["MemberManagement"])
+account = Router(tags=["MemberManagement"])
 
 
 # register page render router
-@register.get("/", response=RegisterResponse)
-def get_register_page(request: HttpRequest) -> HttpResponse:
+@account.get("/", response=RegisterResponse)
+def get_register_page(request: HttpRequest) ->HttpResponse:
     return render(request, "register.html")
