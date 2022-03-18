ac4750cf9fcfbe5fe94e8d147f0283491b030089
asd <pa6712@naver.com>
2022-03-18 16:06:27 +0900

    hy,j,y,complete

--- content_post/apis/v1/detail_router.py
+++ content_post/apis/v1/detail_router.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from ninja import Router
@@ -14,93 +15,76 @@ from content_post.apis.v1.schemas.schema_test import UserSchema
 content = Router(tags=["Content_CRUD"])
 
 
-@content.post("/scrap/{feed_id}/",url_name='scrap')
+@content.post("/scrap/{feed_id}/", url_name='scrap')
+@login_required(login_url='/login/')
 def scrap(request: HttpRequest, feed_id: int) -> HttpResponse:
-    # 여기부터는 로그인을 위해 잠깐 넣었습니다
-    username = 'seo'
-    password = '123'
-    me = auth.authenticate(request, username=username, password=password)
-    if me is not None:
-        auth.login(request, me)
-        # 여기까지는 로그인을위해서
-        user_id = request.user.id
-        # 로그인한 유저의 정보가져오기
-        user = User.objects.get(id=user_id)
-        # user = 로그인한 객체
-        feed = Feeds.objects.get(id=feed_id)
-        # feed = 스크랩할 피드의 객체
-        exist_check = feed.scrape.filter(id=user_id)
-        #scrape필드에 로그인한 유저가있는지 체크
-        if exist_check.exists():
-            #만약 있다면
-            Feeds.objects.get(id=feed_id).scrape.remove(user)
-            #scrape필드에서 유저를 제거
-            feed.scrapes -= 1
-            #스크랩카운트 -1
-            feed.save()
-            #저장
-            scrap_count = feed.scrapes
-            #저장된 스크립카운트
-            ajax = {'scrap_count':scrap_count}
-            #딕셔너리형태로 제이슨으로 보낸다
-            return JsonResponse(ajax)
-        else:
-            #없다면
-            Feeds.objects.get(id=feed_id).scrape.add(user)
-            #scrape필드에 유저를 추가
-            feed.scrapes += 1
-            #스크랩카운트+1
-            feed.save()
-            #저장
-            scrap_count = feed.scrapes
-            #저장된카운트
-            ajax = {'scrap_count': scrap_count}
-            # 딕셔너리형태로 제이슨으로 보낸다
-            return JsonResponse(ajax)
+    user_id = request.user.id
+    # 로그인한 유저의 정보가져오기
+    user = User.objects.get(id=user_id)
+    # user = 로그인한 객체
+    feed = Feeds.objects.get(id=feed_id)
+    # feed = 스크랩할 피드의 객체
+    exist_check = feed.scrape.filter(id=user_id)
+    # scrape필드에 로그인한 유저가있는지 체크
+    if exist_check.exists():
+        # 만약 있다면
+        Feeds.objects.get(id=feed_id).scrape.remove(user)
+        # scrape필드에서 유저를 제거
+        feed.scrapes -= 1
+        # 스크랩카운트 -1
+        feed.save()
+        # 저장
+        scrap_count = feed.scrapes
+        # 저장된 스크립카운트
+        ajax = {'scrap_count': scrap_count}
+        # 딕셔너리형태로 제이슨으로 보낸다
+        return JsonResponse(ajax)
+    else:
+        # 없다면
+        Feeds.objects.get(id=feed_id).scrape.add(user)
+        # scrape필드에 유저를 추가
+        feed.scrapes += 1
+        # 스크랩카운트+1
+        feed.save()
+        # 저장
+        scrap_count = feed.scrapes
+        # 저장된카운트
+        ajax = {'scrap_count': scrap_count}
+        # 딕셔너리형태로 제이슨으로 보낸다
+        return JsonResponse(ajax)
 
 
 @content.delete("/scrap/{feed_id}/")
+@login_required(login_url='/login/')
 def scrap_off(request: HttpRequest, feed_id: int, user_id: int) -> HttpResponse:
     Feeds.objects.get(id=feed_id).scrape.remove(User.objects.get(id=user_id))
     return redirect('/')
 
 
-# detail page render router
-@content.get("", response=DetailResponse)
-def get_detail_page(request: HttpRequest) -> HttpResponse:
-    # print(user.id)
-    return render(request, "detail.html")
-
-
-@content.get("/{feed_id}/", response=List[DetailResponse])
-def get_detail_page(request: HttpRequest, feed_id: int) -> HttpResponse:
-    # 로그인을위해 잠시 입력
-    username = 'tester1234'
-    password = '1234'
-    me = auth.authenticate(request, username=username, password=password)
-    if me is not None:
-        auth.login(request, me)
-        #로그인을 위해 잠시입력
-        user_id = request.user.id
-        # 로그인된 유저의 아이디값
-        feed = Feeds.objects.get(id=feed_id)
-        # 디테일 페이지에 뿌려질 피드 객체
-        check = feed.scrape.filter(id=user_id)
-        # 로그인된 유저의 값으로 피드에 스크랩했는지 체크
-        comments = Comments.objects.filter(feed_id=feed_id).order_by('-created_at')
-        # 피드에 달린 댓글 객체들
-        if check.exists():
-            # 만약 스크랩을 했다면
-            return render(request, "detail.html", {'feed': feed, 'comments': comments, 'scraped': 'scraped'})
-        else:
-            # 스크랩을 안했다면
-            return render(request, "detail.html", {'feed': feed, 'comments': comments})
-
-
 # detail/feeds page render router
 @content.get("/feeds/", response=DetailResponse)
+@login_required(login_url='/login/')
 def get_feeds_page(request: HttpRequest) -> HttpResponse:
     return render(request, "add.html")
+
+
+# 상세 FEED페이지로 이동
+@content.get("/{feed_id}/", response=List[DetailResponse])
+def get_detail_page(request: HttpRequest, feed_id: int) -> HttpResponse:
+    user_id = request.user.id
+    # 로그인된 유저의 아이디값
+    feed = Feeds.objects.get(id=feed_id)
+    # 디테일 페이지에 뿌려질 피드 객체
+    check = feed.scrape.filter(id=user_id)
+    # 로그인된 유저의 값으로 피드에 스크랩했는지 체크
+    comments = Comments.objects.filter(feed_id=feed_id).order_by('-created_at')
+    # 피드에 달린 댓글 객체들
+    if check.exists():
+        # 만약 스크랩을 했다면
+        return render(request, "detail.html", {'feed': feed, 'comments': comments, 'scraped': 'scraped'})
+    else:
+        # 스크랩을 안했다면
+        return render(request, "detail.html", {'feed': feed, 'comments': comments})
 
 # # 수정페이지 이동 변경예정
 # @detail.get('/feeds/', response=DetailResponse)
--- user_admission/apis/v1/login_router.py
+++ user_admission/apis/v1/login_router.py
@@ -10,28 +10,28 @@ account = Router(tags=["MemberManagement"])
 
 
 # login page render router
-<<<<<<< HEAD
-=======
+
 # @account.get("/", response=LoginResponse)
 # def get_login_page(request: HttpRequest) -> HttpResponse:
 #     return render(request, "login.html")
 
 # 경로가 ~ login/ 이고(/다음에 아무것도 없을때) get이면 위의 함수를 실행해라
 
->>>>>>> 80a84a1a754f660c554a17d92afe67728e3b1244
+
 @account.get("/", url_name='login', response=LoginResponse)
 def get_login_page(request: HttpRequest) -> HttpResponse:
-
     return render(request, "login.html")
+
 
 @account.post("/", url_name='login', response=LoginResponse)
 def post_login_page(request: HttpRequest) -> HttpResponse:
     username = request.POST['username']
     password = request.POST['password']
 
+
     user = auth.authenticate(
         request, username=username, password=password
-        )
+    )
 
     if user is not None:
         auth.login(request, user)
@@ -39,5 +39,5 @@ def post_login_page(request: HttpRequest) -> HttpResponse:
     else:
         return render(request, "login.html", {
             'error': 'Username or Password is incorrect.',
-            })
+        })
     return render(request, "login.html")
