91918167c9fe98e164ac1d8395eca75b1aac8eab
asd <pa6712@naver.com>
2022-03-25 13:52:50 +0900

    test

--- content_post/apps.py
+++ content_post/apps.py
@@ -8,9 +8,9 @@ class ContentPostConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "content_post"
     url = "add.xlsx"
-    # exel = pd.read_excel(url, engine='openpyxl')
-    #
-    # @staticmethod
-    # def get_exel(self) -> DataFrame:
-    #     url = "add.xlsx"
-    #     return pd.read_excel(url, engine='openpyxl')
+    exel = pd.read_excel(url, engine='openpyxl')
+
+    @staticmethod
+    def get_exel(self) -> DataFrame:
+        url = "add.xlsx"
+        return pd.read_excel(url, engine='openpyxl')
