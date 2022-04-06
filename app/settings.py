ee3f372c32c7d07e6b3e83b9541451de6b2c2b8d
Ubuntu <ubuntu@ip-172-31-42-170.ap-northeast-2.compute.internal>
2022-04-06 13:19:08 +0000

    sorry

--- app/settings.py
+++ app/settings.py
@@ -181,7 +181,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# print(STATIC_ROOT)
 MEDIA_URL = "https://s3.console.aws.amazon.com/s3/buckets/team05-tomorrow-clothes?region=ap-northeast-2&tab=objects/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "/img/upload/")
 # MEDIA_URL = "/media/"
