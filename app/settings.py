0b517ac151f29abe4040e0ada83cca460b6ca706
asd <pa6712@naver.com>
2022-04-07 17:48:42 +0900

    test

--- app/settings.py
+++ app/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = TEAM5_SECRET["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS: List[str] = ["*"]
 
